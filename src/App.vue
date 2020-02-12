be21afb6b5ed36bb35035b0dd76911b6d0a38724
lovelag <jbernlove@gmail.com>
2020-02-12 11:04:22 +0800

    c

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div style="margin:10% 25%;">
+   <ejs-autocomplete></ejs-autocomplete>
+   </div>
+  </div>
+</template>
+
+<script>
+
+import Vue from "vue";
+import {AutoCompletePlugin} from "@syncfusion/ej2-vue-dropdowns";
+Vue.use(AutoCompletePlugin);
+export default Vue.extend ({
+  data: function(){
+    return{
+
+    };
+  }
+})
+</script>
+
+<style>
+@import url(https://cdn.syncfusion.com/ej2/material.css);
+
+</style>
+
